33d8bb3a5219f7c1219817bf2a0a23f83fa2e112
Hoon Park <hoonpark@Hoons-MacBook-Pro.local>
2021-04-12 00:55:30 -0700

    write functions

--- inc02b.R
+++ inc02b.R
@@ -3,5 +3,18 @@ lightMinute <- c*60
 lightHour <- lightMinute*60
 lightDay <- lightHour*24
 lightYear <- lightDay*365
-# 
+cat("length of light year:", lightYear, "\n", sep=" ")
+cat("That's too long")
 
+add <- function(x,y) {
+  x + y
+}
+
+lightyear <- function(ly) {
+  c <- 300000
+  lightMinute <- c*60
+  lightHour <- lightMinute*60
+  lightDay <- lightHour*24
+  lightYear <- lightDay*365
+  lightYear*ly
+}
